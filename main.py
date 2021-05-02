0af09d61b8cd8cf5fcb40dc48f1ff069c649e9d6
BuildTools <unconfigured@null.spigotmc.org>
2021-05-02 03:28:32 -0500

    object serialized

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ async def create_user(user: UserRequestModel):
         username=user.username,
         password=hash_password
     )
-    return UserResponseModel(id=user.id, username=user.username)
+    return user
